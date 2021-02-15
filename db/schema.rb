d57c059287ee23ea7331cfa3c31cc7c263c481a1
Valentin Raille <Valrail78@gmail.com>
2021-02-15 23:59:17 +0100

    Switched to postgresl for deployment on Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_08_08_160439) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookings", force: :cascade do |t|
     t.string "date"
     t.integer "speedboat"
