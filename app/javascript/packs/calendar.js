3e2220b3267fc817f9adde6315aa77379c05f97e
Valentin Raille <Valrail78@gmail.com>
2021-02-17 16:10:49 +0100

    deploying for review

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,7 +1,6 @@
 import "@fortawesome/fontawesome-free/js/all";
 import flatpickr from "flatpickr";
 import { Korean } from "flatpickr/dist/l10n/ko.js";
-import "flatpickr/dist/flatpickr.css";
 
 console.log(document.getElementById('calendar-target'))
 let userdate = document.getElementById('calendar-target').flatpickr({
@@ -19,3 +18,4 @@ let userdate = document.getElementById('calendar-target').flatpickr({
 document.querySelector(".form-control").style.display = "none";
 
 
+   
